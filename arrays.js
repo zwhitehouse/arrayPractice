6f3ef0c942014fe579704eb17e7164fb4208c5df
Zack Whitehouse <zackwhitehouse225@gmail.com>
2023-08-01 21:06:09 -0400

    added methods

--- arrays.js
+++ arrays.js
@@ -1,3 +1,14 @@
+
 var one = ['harold', 'david', 'john', 'peter'];
 var two = [45, 78, 24, 88];
 var three = [true, false, true, false];
+
+one.pop();
+// This method will take the last element off the end of the array and return it as a value
+
+two.push();
+// This method will push on a new element to the end of the array
+
+three.shift();
+// This method will remove the first element in an array and return it as a value
+
